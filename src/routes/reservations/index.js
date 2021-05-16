e4acb2c73f4151ef43bb697ee68c909ab5784b41
Fernando Carmona <fernando.carayu@gmail.com>
2021-05-16 16:02:38 +0200

    change file name

--- src/routes/reservations/index.js
+++ src/routes/reservations/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 module.exports = (db) => {
-  router.get("/", require("./activeCount")(db));
+  router.get("/", require("./getActiveCount")(db));
 
   return router;
 };
