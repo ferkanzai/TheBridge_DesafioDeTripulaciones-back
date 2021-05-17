846cb95bdd85737b847d4b4ab94dbb9b2dd1d10a
Fernando Carmona <fernando.carayu@gmail.com>
2021-05-17 16:57:55 +0200

    dates working

--- src/routes/reservations/postStartReservation.js
+++ src/routes/reservations/postStartReservation.js
@@ -24,12 +24,23 @@ module.exports = (db) => async (req, res, next) => {
       connectionId
     );
 
-    const now = new Date();
+    const now = new Date(Date.now()).toUTCString();
     const expiration_date = new Date(
       isConnectionReserved.rows[0]?.expiration_date
     );
+    const expiration_date_UTC = new Date(
+      expiration_date.getTime() -
+        expiration_date.getTimezoneOffset() * 60 * 1000
+    ).toUTCString();
+    //isConnectionReserved.rows[0]?.expiration_date
+    console.log(expiration_date.getTimezoneOffset());
+    console.log(now);
+    console.log(expiration_date);
+    console.log(expiration_date_UTC);
 
-    if (isConnectionReserved.rowCount && now > expiration_date) {
+    console.log(now > expiration_date_UTC);
+
+    if (!!isConnectionReserved.rowCount && now < expiration_date_UTC) {
       const error = new Error(
         "Unable to create new reservation, connection already reserved"
       );
@@ -44,6 +55,9 @@ module.exports = (db) => async (req, res, next) => {
     }
 
     const { rows, rowCount } = result;
+
+    rows[0].reservation_date = now;
+    rows[0].expiration_date = expiration_date_UTC;
 
     res.status(200).json({
       success: true,
