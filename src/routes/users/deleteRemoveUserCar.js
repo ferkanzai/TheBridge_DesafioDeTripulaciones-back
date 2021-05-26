db227083a18f73fe4aaa08608ec729bfc3dc1f86
Fernando Carmona <fernando.carayu@gmail.com>
2021-05-26 21:41:16 +0200

    error control

--- src/routes/users/deleteRemoveUserCar.js
+++ src/routes/users/deleteRemoveUserCar.js
@@ -7,19 +7,16 @@ module.exports = (db) => async (req, res, next) => {
   try {
     const result = await deleteRemoveUserCar(db, id, carId);
 
-    if (!result) {
-      const error = new Error("Trying to delete a row that does not exist");
-      error.code = 400;
-      next(error);
-      return;
+    if (result instanceof Error) {
+      throw result;
     }
+
+    const { rows, rowCount } = result;
 
     res.status(200).json({
       success: true,
-      data: {
-        user_id: id,
-        car_id: Number(carId),
-      },
+      count: rowCount,
+      data: rows,
     });
   } catch (error) {
     console.info("> something went wrong: ", error.message);
